c8d24e2ff99230b66d0576443111361d1e9e14ac
Maartje Eyskens <meyskens@me.com>
2018-07-29 11:54:35 +0200

    Add login to database

--- components/data/users.js
+++ components/data/users.js
@@ -1,9 +1,10 @@
 import mongoose from "../db"
+import bcrypt from "bcrypt"
 
 const UsersSchema = new mongoose.Schema({
     name: String,
     email: String,
-    passwordHash: Object,
+    passwordHash: String,
 }, { collection: "users" })
 UsersSchema.index({
     repo: 1,
@@ -11,8 +12,37 @@ UsersSchema.index({
 
 const UsersModel = mongoose.model("users", UsersSchema, "users")
 
+const saltRounds = 10;
+
 export const getUser = (id) => {
     return UsersModel.findOne({
         _id: mongoose.Schema.Types.ObjectId(id),
     }).exec()
+}
+
+export const getUserForEmail = (email) => {
+    return UsersModel.findOne({
+        email,
+    }).exec()
+}
+
+export const createUser = async ({ name, email, password }) => {
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+    return (new UsersModel({ name, email, passwordHash })).save()
+}
+
+export const checkLogin = async (email, password) => {
+    const user = await getUserForEmail(email)
+    if (!user) {
+        await bcrypt.compare(password, "$2b$10$3zPZXXXqVQjcFymS7Hww6.ONzvZ3KNP0tHZqXJs0Xa71ddzcoyTcK"); // this makes the request take as long as if a user was found to prevent brute force of users
+        return false
+    }
+
+    const match = await bcrypt.compare(password, user.passwordHash)
+    
+    if (match) {
+        return user
+    }
+    
+    return false
 }
