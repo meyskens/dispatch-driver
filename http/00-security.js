009eeb811d22ea3c90721c74dd0bf6b163debfbf
Maartje Eyskens <meyskens@me.com>
2018-07-29 20:53:08 +0200

    add /dash/me

--- http/00-security.js
+++ http/00-security.js
@@ -1,45 +1,45 @@
 import fs from "fs"
 import * as apps from "~/components/data/apps"
 import * as users from "~/components/data/users"
-import BadRequestError from "~/http/classes/badrequest";
+import BadRequestError from "~/http/classes/badrequest"
 import AccessDeniedError from "~/http/classes/accessdenied"
 import UnauthorizedError from "~/http/classes/unauthorized" 
 
-const publicKey = fs.readFileSync("./keys/publicKey.pem");
-const cert = fs.readFileSync("./keys/signingKey.pem");
+const publicKey = fs.readFileSync("./keys/publicKey.pem")
+const cert = fs.readFileSync("./keys/signingKey.pem")
 
 export default function ({ app, expressJWT, wrap, jwt }) {
     app.use("/dash", expressJWT({
         secret: publicKey,
         audience: "https://driver.dispatch.sh/dash",
-    }));
+    }))
 
     app.all("/dash/*", wrap(async (req, res, next) => {
         if (!req.query.app && !req.body.app) {
-            return next();
+            return next()
         }
 
-        const appToCheck = req.query.app || req.body.app;
+        const appToCheck = req.query.app || req.body.app
         const app = apps.get(appToCheck)
         if (!app) {
-            throw new BadRequestError("The app mentioned in the request isn't valid.");
+            throw new BadRequestError("The app mentioned in the request isn't valid.")
         }
 
         if (app.user != req.user._id) {
             throw new AccessDeniedError("The app belongs to another user.")
         }
 
-        req.app = app;
-        return next(); // allow request to continue
-    }));
+        req.app = app
+        return next() // allow request to continue
+    }))
 
 
     app.post("/authenticate", wrap(async (req, res) => {
         if ( !req.body.email || !req.body.password) {
-            throw new BadRequestError("Missing data");
+            throw new BadRequestError("Missing data")
         }
 
-        const user = await users.checkLogin(req.body.email, req.body.password);
+        const user = await users.checkLogin(req.body.email, req.body.password)
         if (!user) {
             throw new UnauthorizedError("Wrong login")
         }
@@ -52,13 +52,13 @@ export default function ({ app, expressJWT, wrap, jwt }) {
             expiresIn: "60m",
             audience: "https://driver.dispatch.sh/dash",
             algorithm: "RS256",
-        });
+        })
     
-        res.json({ token });
-    }));
+        res.json({ token })
+    }))
 
     app.post("/dash/keep-alive", wrap(async (req, res) => {
-        const user = await users.get(req.user._id);
+        const user = await users.get(req.user._id)
         const token = jwt.sign({
             email: user.email,
             name: user.name,
@@ -67,7 +67,11 @@ export default function ({ app, expressJWT, wrap, jwt }) {
             expiresIn: "60m",
             audience: "https://driver.dispatch.sh/dash",
             algorithm: "RS256",
-        });
-        res.json({ token });
-    }));
+        })
+        res.json({ token })
+    }))
+    
+    app.get("/dash/me", wrap(async (req,res) => {
+        res.json(await users.get(req.user._id))
+    }))
 }
