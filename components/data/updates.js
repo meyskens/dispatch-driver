d692c6ab869c2dbfa4885a01e35f4dcba3facf9a
Maartje Eyskens <meyskens@me.com>
2018-07-30 11:36:59 +0200

    add updates

---
+++ components/data/updates.js
@@ -0,0 +1,22 @@
+import mongoose from "./db"
+
+const ObjectId = mongoose.Types.ObjectId
+const Schema = mongoose.Schema
+const UpdatesSchema = new mongoose.Schema({
+    tag: String,
+    oldTag: String,
+    time: Date,
+    app: {
+        type: Schema.Types.ObjectId,
+        ref: "apps",
+    },
+}, { collection: "updates" })
+UpdatesSchema.index({
+    app: 1,
+})
+
+const UpdatesModel = mongoose.model("updates", UpdatesSchema, "updates")
+
+export const getForApp = (app) => {
+    return UpdatesModel.find({ app: new ObjectId(app) }).exec()
+}
