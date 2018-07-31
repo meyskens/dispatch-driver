3a2a87b1551715d8f149a412f84d75bda8aafbf0
Maartje Eyskens <meyskens@me.com>
2018-07-31 08:50:43 +0200

    Add envvars and files

---
+++ components/data/envvars.js
@@ -0,0 +1,18 @@
+import mongoose from "../db"
+
+const ObjectId = mongoose.Types.ObjectId
+const EnvVarSchema = new mongoose.Schema({
+    name: String,
+    key: String,
+    value: String,
+    app: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "apps",
+    },
+}, { collection: "envvars" })
+EnvVarSchema.index({
+    app: 1,
+    user: 1,
+})
+
+const EnvVarModel = mongoose.model("envvars", EnvVarSchema, "envvars")
---
+++ components/data/files.js
@@ -0,0 +1,17 @@
+import mongoose from "../db"
+
+const ObjectId = mongoose.Types.ObjectId
+const FilesSchema = new mongoose.Schema({
+    name: String,
+    path: String,
+    content: String, // base64 encoded
+    app: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "apps",
+    },
+}, { collection: "files" })
+FilesSchema.index({
+    app: 1,
+})
+
+const FilesModel = mongoose.model("files", FilesSchema, "files")
