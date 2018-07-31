90b97d57ea7c3efe2a165c949dcd1bf74dc8991f
Maartje Eyskens <meyskens@me.com>
2018-07-31 17:42:10 +0200

    add getFilesForApp

--- components/data/files.js
+++ components/data/files.js
@@ -15,3 +15,9 @@ FilesSchema.index({
 })
 
 const FilesModel = mongoose.model("files", FilesSchema, "files")
+
+export const getFilesForApp = (app) => {
+    return FilesModel.find({
+        app: ObjectId(app),
+    })
+}
