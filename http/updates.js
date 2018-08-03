3599583da3e0c52d30b95585acf0e9259549ae72
Maartje Eyskens <meyskens@me.com>
2018-08-03 22:13:47 +0200

    fix updates

--- http/updates.js
+++ http/updates.js
@@ -4,6 +4,6 @@ import { NotFoundError } from "~/http/classes/notfound"
 
 export default function({ app, wrap }) {
     app.get("/dash/app/:id/updates", wrap(async (req, res) => {
-        return res.json(await updates.getLastForApp(app._id))
+        return res.json(await updates.getLastForApp(req.params.id))
     }))
 }
