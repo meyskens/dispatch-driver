8d3229d0523c92ef8b828a0d354fc9f6a4a17320
Maartje Eyskens <meyskens@me.com>
2018-08-03 16:11:42 +0200

    add envvars

--- components/data/envvars.js
+++ components/data/envvars.js
@@ -1,7 +1,7 @@
 import mongoose from "../db"
 
 const ObjectId = mongoose.Types.ObjectId
-const EnvVarSchema = new mongoose.Schema({
+const EnvVarsSchema = new mongoose.Schema({
     name: String,
     key: String,
     value: String,
@@ -11,9 +11,35 @@ const EnvVarSchema = new mongoose.Schema({
         ref: "apps",
     },
 }, { collection: "envvars" })
-EnvVarSchema.index({
+EnvVarsSchema.index({
     app: 1,
     user: 1,
 })
 
-const EnvVarModel = mongoose.model("envvars", EnvVarSchema, "envvars")
+const EnvVarsModel = mongoose.model("envvars", EnvVarsSchema, "envvars")
+
+export const getForApp = (app) => {
+    return EnvVarsModel.find({
+        app: ObjectId(app),
+    })
+}
+
+export const getForAppAndID = (app, id) => {
+    return EnvVarsModel.find({
+        _id: ObjectId(id),
+        app: ObjectId(app),
+    })
+}
+
+export const addForApp = (appID, entry) => {
+    entry.app = ObjectId(appID)
+    return (new EnvVarsModel(entry)).save()
+}
+
+export const update = (id, entry) => {
+    return EnvVarsModel.update({ _id: ObjectId(id) }, entry).exec()
+}
+
+export const remove = (id) => {
+    return EnvVarsModel.remove({ _id: ObjectId(id) }).exec()
+}
---
+++ http/envvars.js
@@ -0,0 +1,31 @@
+import * as apps from "~/components/data/apps"
+import * as envvars from "~/components/data/envvars"
+import { NotFoundError } from "~/http/classes/notfound"
+
+export default function({ app, wrap }) {
+    app.get("/dash/app/:id/envvars", wrap(async (req, res) => {
+        return res.json(await envvars.getForApp(req.params.id))
+    }))
+
+    app.post("/dash/app/:id/envvar", wrap(async (req, res) => {
+        return res.json(await envvars.addForApp(req.params.id, req.body))
+    }))
+
+    app.patch("/dash/app/:id/envvar/:envvarid", wrap(async (req, res) => {
+        const file = await envvars.getForAppAndID(req.params.id, req.params.envvarid)
+        if (!file) {
+            throw new NotFoundError("Env var not found")
+        }
+        await envvars.update(req.params.envvarid, req.body)
+        return res.json({ "status": "ok" })
+    }))
+
+    app.delete("/dash/app/:id/envvar/:envvarid", wrap(async (req, res) => {
+        const file = await envvars.getForAppAndID(req.params.id, req.params.envvarid)
+        if (!file) {
+            throw new NotFoundError("Env var not found")
+        }
+        await envvars.remove(req.params.envvarid)
+        return res.json({ "status": "ok" })
+    }))
+}
